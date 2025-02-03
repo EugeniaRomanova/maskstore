39213dd390cfcd5d58ca8433afe27dde5c7aeb71
EugeniaRomanova <zubrsonnyj@gmail.com>
2025-02-03 10:09:41 +0300

    Переводит подключение БД с библиотеки mysqli на PDO

--- config/connect.php
+++ config/connect.php
@@ -1,7 +1,25 @@
 <?php
-$connect = mysqli_connect('localhost', 'cr11847_mal', '5H6t2eUH', 'malcorpse');
+/*$connect = mysqli_connect('localhost', 'cr11847_mal', '5H6t2eUH', 'malcorpse');
 
 if (!$connect) {
   die('Нет подключения к базе данных');
+}
+require_once 'setting.php';
+$connection = new PDO("mysql:host=localhost;dbname=malcorpse", "cr11847_mal", "5H6t2eUH");*/
+$driver = 'mysql';
+$host = 'localhost';
+$db_name = 'malcorpse';
+$db_user = 'cr11847_mal';
+$db_pass = '5H6t2eUH';
+$charset = 'utf8';
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+try{
+  $pdo = new PDO(
+    "$driver:host=$host;dbname=$db_name;charset=$charset", 
+    $db_user, $db_pass, $options
+  );
+} catch (PDOException $i){
+  die("Ошибка подключения к базе данных");
 }
 ?>
---
+++ config/setting.php
@@ -0,0 +1,6 @@
+<?php 
+$host = 'localhost';
+$data = 'malcorpse';
+$user = 'cr11847_mal';
+$pass = '5H6t2eUH';
+?>
